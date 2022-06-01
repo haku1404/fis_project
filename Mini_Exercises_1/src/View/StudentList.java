7abd6594e2aca7fed5cbb244cd86b4f173831c93
ichihaku14 <hieunkhe140250@fpt.edu.vn>
2022-06-01 09:13:04 +0700

    updated

--- Mini_Exercises_1/src/View/StudentList.java
+++ Mini_Exercises_1/src/View/StudentList.java
@@ -17,20 +17,20 @@ public class StudentList {
     static Student st = new Student();
     static List<Student> list = new ArrayList<>();
     public void addStudent() throws ParseException {
-        System.out.println("moi ban nhap vao code :");
+        System.out.println("Enter Student Code: ");
         try {
             st.setCode(scanner.nextInt());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        System.out.println("moi ban nhap vao name :");
+        System.out.println("Enter Student Name: ");
         scanner.nextLine();
         try {
             st.setName(scanner.nextLine());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        System.out.println("moi ban nhap vao dinh dang: dd/mm/yyyy :");
+        System.out.println("Enter Student BirthDate(dd/MM/yyyy): ");
         try {
             st.setBirthDate((Date) new SimpleDateFormat("dd/MM/yyyy").parse(scanner.nextLine()));
         } catch (Exception e) {
