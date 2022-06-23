414a385c1c09224a1aab60686264a1e2b89fb753
ichihaku14 <hieunkhe140250@fpt.edu.vn>
2022-06-23 22:43:32 +0700

    frame uploaded

---
+++ Sprint04_Criminal/criminal-system/criminal-system/src/main/java/fis/criminal/criminalsystem/CriminalSystemApplication.java
@@ -0,0 +1,13 @@
+package fis.criminal.criminalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriminalSystemApplication.class, args);
+	}
+
+}
---
+++ Sprint04_Criminal/criminal-system/criminal-system/src/test/java/fis/criminal/criminalsystem/CriminalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fis.criminal.criminalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriminalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
