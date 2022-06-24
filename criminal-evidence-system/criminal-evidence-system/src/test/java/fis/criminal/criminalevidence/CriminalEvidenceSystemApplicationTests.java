5106e91dcb762f63f0465856d8bc18b4ae113942
ichihaku14 <hieunkhe140250@fpt.edu.vn>
2022-06-24 23:10:03 +0700

    modified

---
+++ criminal-evidence-system/criminal-evidence-system/src/test/java/fis/criminal/criminalevidence/CriminalEvidenceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fis.criminal.criminalevidence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriminalEvidenceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
